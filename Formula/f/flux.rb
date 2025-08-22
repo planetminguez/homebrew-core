4b9ec409f725e561c912a9448ab175694c6c8376
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-22 17:04:38 +0000

    flux 0.198.0

--- Formula/f/flux.rb
+++ Formula/f/flux.rb
@@ -2,8 +2,8 @@ class Flux < Formula
   desc "Lightweight scripting language for querying databases"
   homepage "https://www.influxdata.com/products/flux/"
   url "https://github.com/influxdata/flux.git",
-      tag:      "v0.197.0",
-      revision: "6f5f1c0c24c7da7a705f9805c2782ba091599c5f"
+      tag:      "v0.198.0",
+      revision: "432969ffee5365e35d7519884e0b4d8e56cce01b"
   license "MIT"
   head "https://github.com/influxdata/flux.git", branch: "master"
 
